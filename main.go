5a515b129172e5401157435d064303d32b490a5e
Alex Zhukov <baron.pampa@gmail.com>
2016-02-11 00:06:50 +0300

    gofmt everything

--- main.go
+++ main.go
@@ -1,252 +1,251 @@
 package main
 
 import (
-  "os"
-  "github.com/codegangsta/cli"
-  "github.com/cheggaaa/pb"
-  "fmt"
-  "time"
+	"fmt"
+	"github.com/cheggaaa/pb"
+	"github.com/codegangsta/cli"
+	"os"
+	"time"
 )
 
 var r rutil
 
 func main() {
-  app := cli.NewApp()
-  app.Usage   = "redis multitool utility"
-  app.Version = "0.1.0"
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name: "host, s",
-      Value: "127.0.0.1",
-      Usage: "redis host",
-    },
-    cli.StringFlag{
-      Name: "auth, a",
-      Usage: "authentication password",
-    },
-    cli.IntFlag {
-      Name: "port, p",
-      Value: 6379,
-      Usage:"redis port",
-    },
-  }
+	app := cli.NewApp()
+	app.Usage = "redis multitool utility"
+	app.Version = "0.1.0"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "host, s",
+			Value: "127.0.0.1",
+			Usage: "redis host",
+		},
+		cli.StringFlag{
+			Name:  "auth, a",
+			Usage: "authentication password",
+		},
+		cli.IntFlag{
+			Name:  "port, p",
+			Value: 6379,
+			Usage: "redis port",
+		},
+	}
 
-  app.Before = func (c *cli.Context) error {
-    r.Host = c.GlobalString("host")
-    r.Port = c.GlobalInt("port")
-    r.Auth = c.GlobalString("auth")
-    return nil
-  }
+	app.Before = func(c *cli.Context) error {
+		r.Host = c.GlobalString("host")
+		r.Port = c.GlobalInt("port")
+		r.Auth = c.GlobalString("auth")
+		return nil
+	}
 
 	app.Commands = []cli.Command{
-    {
-        Name:        "dump",
-        Usage:       "dump redis database to a file",
-        Flags: []cli.Flag {
-          cli.StringFlag {
-            Name: "keys, k",
-            Value: "*",
-            Usage: "keys pattern (passed to redis 'keys' command)",
-          },
-          cli.BoolFlag {
-            Name: "auto, a",
-            Usage: "make up a file name for the dump - redisYYYYMMDDHHMMSS.rdmp",
-          },
-          cli.StringFlag {
-            Name: "match, m",
-            Usage: "regexp filter for key names",
-          },
-          cli.BoolFlag {
-            Name: "invert, v",
-            Usage:"invert match regexp",
-          },
-        },
-        Action: func(c *cli.Context) {
-          args  := c.Args()
-          auto  := c.Bool("auto")
-          regex := c.String("match")
-          inv   := c.Bool("invert")
+		{
+			Name:  "dump",
+			Usage: "dump redis database to a file",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "keys, k",
+					Value: "*",
+					Usage: "keys pattern (passed to redis 'keys' command)",
+				},
+				cli.BoolFlag{
+					Name:  "auto, a",
+					Usage: "make up a file name for the dump - redisYYYYMMDDHHMMSS.rdmp",
+				},
+				cli.StringFlag{
+					Name:  "match, m",
+					Usage: "regexp filter for key names",
+				},
+				cli.BoolFlag{
+					Name:  "invert, v",
+					Usage: "invert match regexp",
+				},
+			},
+			Action: func(c *cli.Context) {
+				args := c.Args()
+				auto := c.Bool("auto")
+				regex := c.String("match")
+				inv := c.Bool("invert")
 
-          var fileName string
+				var fileName string
 
-          if len(args) == 0 && auto == false  {
-            checkErr("provide a file name for the dump or use rutil dump --auto to make one up")
-          } else if len(args) > 0 && auto == true {
-            checkErr("you can't provide a name and use --auto at the same time")
-          } else if len(args) == 1 && auto == false {
-            fileName = args[0]
-          } else if auto == true {
-            fileName = fmt.Sprintf("redis%s.rdmp", time.Now().Format("20060102150405"))
-          } else if len(args) > 1 {
-            checkErr("to many file names")
-          } else if fileName == "" {
-            checkErr("brain damage. panic")
-          }
+				if len(args) == 0 && auto == false {
+					checkErr("provide a file name for the dump or use rutil dump --auto to make one up")
+				} else if len(args) > 0 && auto == true {
+					checkErr("you can't provide a name and use --auto at the same time")
+				} else if len(args) == 1 && auto == false {
+					fileName = args[0]
+				} else if auto == true {
+					fileName = fmt.Sprintf("redis%s.rdmp", time.Now().Format("20060102150405"))
+				} else if len(args) > 1 {
+					checkErr("to many file names")
+				} else if fileName == "" {
+					checkErr("brain damage. panic")
+				}
 
-          keys, keys_c := r.getKeys(c.String("keys"),regex,inv)
+				keys, keys_c := r.getKeys(c.String("keys"), regex, inv)
 
-          file, err := os.Create(fileName)
-          checkErr(err)
+				file, err := os.Create(fileName)
+				checkErr(err)
 
-          bar := pb.StartNew(keys_c)
+				bar := pb.StartNew(keys_c)
 
-          totalBytes := r.writeHeader(file, keys_c)
+				totalBytes := r.writeHeader(file, keys_c)
 
-          for _, k := range keys {
-            bar.Increment()
-            b := r.writeDump(file, r.dumpKey(k))
-            totalBytes = totalBytes + b
-          }
-          bar.FinishPrint(fmt.Sprintf("file: %s, keys: %d, bytes: %d", fileName, keys_c, totalBytes))
-        },
-    },
-    {
-        Name:        "restore",
-        Usage:       "restore redis database from file",
-        Flags: []cli.Flag {
-          cli.BoolFlag {
-            Name: "dry-run, r",
-            Usage: "pretend to restore",
-          },
-          cli.BoolFlag {
-            Name: "flushdb, f",
-            Usage: "flush the database before restoring",
-          },
-          cli.BoolFlag {
-            Name: "delete, d",
-            Usage: "delete key before restoring",
-          },
-          cli.BoolFlag {
-            Name: "ignore, g",
-            Usage: "ignore BUSYKEY restore errors",
-          },
-        },
-        Action: func(c *cli.Context) {
-          args  := c.Args()
-          dry   := c.Bool("dry-run")
-          flush := c.Bool("flushdb")
-          del   := c.Bool("delete")
-          ignor := c.Bool("ignore")
+				for _, k := range keys {
+					bar.Increment()
+					b := r.writeDump(file, r.dumpKey(k))
+					totalBytes = totalBytes + b
+				}
+				bar.FinishPrint(fmt.Sprintf("file: %s, keys: %d, bytes: %d", fileName, keys_c, totalBytes))
+			},
+		},
+		{
+			Name:  "restore",
+			Usage: "restore redis database from file",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "dry-run, r",
+					Usage: "pretend to restore",
+				},
+				cli.BoolFlag{
+					Name:  "flushdb, f",
+					Usage: "flush the database before restoring",
+				},
+				cli.BoolFlag{
+					Name:  "delete, d",
+					Usage: "delete key before restoring",
+				},
+				cli.BoolFlag{
+					Name:  "ignore, g",
+					Usage: "ignore BUSYKEY restore errors",
+				},
+			},
+			Action: func(c *cli.Context) {
+				args := c.Args()
+				dry := c.Bool("dry-run")
+				flush := c.Bool("flushdb")
+				del := c.Bool("delete")
+				ignor := c.Bool("ignore")
 
-          if flush && del {
-            checkErr("flush or delete?")
-          }
+				if flush && del {
+					checkErr("flush or delete?")
+				}
 
-          if len(args) == 0 {
-            checkErr("no file name provided")
-          } else if len(args) > 1 {
-            checkErr("to many file names")
-          }
+				if len(args) == 0 {
+					checkErr("no file name provided")
+				} else if len(args) > 1 {
+					checkErr("to many file names")
+				}
 
-          file, err := os.Open(args[0])
-          checkErr(err)
-          hd := r.readHeader(file)
+				file, err := os.Open(args[0])
+				checkErr(err)
+				hd := r.readHeader(file)
 
-          if dry == false && flush == true {
-            res := r.Client().Cmd("FLUSHDB")
-            checkErr(res.Err)
-          }
+				if dry == false && flush == true {
+					res := r.Client().Cmd("FLUSHDB")
+					checkErr(res.Err)
+				}
 
-          bar := pb.StartNew(int(hd.Keys))
-          keys_c := 0
-          for i := uint64(0); i < hd.Keys; i++ {
-            bar.Increment()
-            d := r.readDump(file)
-            if dry == false {
-              if dry == false {
-                keys_c = keys_c + r.restoreKey(d, del, ignor)
-              }
-            }
-          }
-          bar.FinishPrint(fmt.Sprintf("file: %s, keys: %d", args[0], keys_c))
-        },
-    },
-    {
-      Name: "delete",
-      Aliases: []string{"del"},
-      Usage: "delete keys matching the pattern provided by --keys",
-      Flags: []cli.Flag {
-          cli.StringFlag {
-            Name: "keys, k",
-            Usage: "keys pattern (passed to redis 'keys' command)",
-          },
-          cli.BoolFlag {
-            Name: "yes",
-            Usage: "really delete keys, default is pretend to delete",
-          },
-          cli.StringFlag {
-            Name: "match, m",
-            Usage: "regexp filter for key names",
-          },
-          cli.BoolFlag {
-            Name: "invert, v",
-            Usage:"invert match regexp",
-          },
-        },
-        Action: func(c *cli.Context) {
-          yes := c.Bool("yes")
-          pat := c.String("keys")
-          regex := c.String("match")
-          inv   := c.Bool("invert")
-          if pat == "" {
-            checkErr("missing --keys pattern")
-          }
+				bar := pb.StartNew(int(hd.Keys))
+				keys_c := 0
+				for i := uint64(0); i < hd.Keys; i++ {
+					bar.Increment()
+					d := r.readDump(file)
+					if dry == false {
+						if dry == false {
+							keys_c = keys_c + r.restoreKey(d, del, ignor)
+						}
+					}
+				}
+				bar.FinishPrint(fmt.Sprintf("file: %s, keys: %d", args[0], keys_c))
+			},
+		},
+		{
+			Name:    "delete",
+			Aliases: []string{"del"},
+			Usage:   "delete keys matching the pattern provided by --keys",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "keys, k",
+					Usage: "keys pattern (passed to redis 'keys' command)",
+				},
+				cli.BoolFlag{
+					Name:  "yes",
+					Usage: "really delete keys, default is pretend to delete",
+				},
+				cli.StringFlag{
+					Name:  "match, m",
+					Usage: "regexp filter for key names",
+				},
+				cli.BoolFlag{
+					Name:  "invert, v",
+					Usage: "invert match regexp",
+				},
+			},
+			Action: func(c *cli.Context) {
+				yes := c.Bool("yes")
+				pat := c.String("keys")
+				regex := c.String("match")
+				inv := c.Bool("invert")
+				if pat == "" {
+					checkErr("missing --keys pattern")
+				}
 
-          keys, _ := r.getKeys(pat,regex,inv)
+				keys, _ := r.getKeys(pat, regex, inv)
 
-          for i, k := range keys {
-            fmt.Printf("%3d: %s\n",i + 1,k)
-            if yes == true {
-              res := r.Client().Cmd("DEL", k)
-              checkErr(res.Err)
-            }
-          }
-        },
-    },
-    {
-      Name: "print",
-      Aliases: []string{"pp"},
-      Usage: "print keys matching the pattern provided by --keys",
-      Flags: []cli.Flag {
-          cli.StringFlag {
-            Name: "keys, k",
-            Usage: "keys pattern (passed to redis 'keys' command)",
-          },
-          cli.StringFlag {
-            Name: "match, m",
-            Usage: "regexp filter for key names",
-          },
-          cli.BoolFlag {
-            Name: "invert, v",
-            Usage:"invert match regexp",
-          },
-          cli.StringSliceFlag {
-            Name: "field, f",
-            Usage:"hash fields to print (default all)",
-          },
-          cli.BoolFlag {
-            Name: "json, j",
-            Usage:"attempt to parse and pretty print strings as json",
-          },
-        },
-        Action: func(c *cli.Context) {
-          pat := c.String("keys")
-          regex := c.String("match")
-          inv   := c.Bool("invert")
+				for i, k := range keys {
+					fmt.Printf("%3d: %s\n", i+1, k)
+					if yes == true {
+						res := r.Client().Cmd("DEL", k)
+						checkErr(res.Err)
+					}
+				}
+			},
+		},
+		{
+			Name:    "print",
+			Aliases: []string{"pp"},
+			Usage:   "print keys matching the pattern provided by --keys",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "keys, k",
+					Usage: "keys pattern (passed to redis 'keys' command)",
+				},
+				cli.StringFlag{
+					Name:  "match, m",
+					Usage: "regexp filter for key names",
+				},
+				cli.BoolFlag{
+					Name:  "invert, v",
+					Usage: "invert match regexp",
+				},
+				cli.StringSliceFlag{
+					Name:  "field, f",
+					Usage: "hash fields to print (default all)",
+				},
+				cli.BoolFlag{
+					Name:  "json, j",
+					Usage: "attempt to parse and pretty print strings as json",
+				},
+			},
+			Action: func(c *cli.Context) {
+				pat := c.String("keys")
+				regex := c.String("match")
+				inv := c.Bool("invert")
 
-          if pat == "" {
-            checkErr("missing --keys pattern")
-          }
+				if pat == "" {
+					checkErr("missing --keys pattern")
+				}
 
-          keys, _ := r.getKeys(pat,regex,inv)
+				keys, _ := r.getKeys(pat, regex, inv)
 
-          for _, k := range keys {
-            r.printKey(k, c.StringSlice("field"),c.Bool("json"))
-          }
-        },
-    },
-  }
+				for _, k := range keys {
+					r.printKey(k, c.StringSlice("field"), c.Bool("json"))
+				}
+			},
+		},
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
-
--- rutil.go
+++ rutil.go
@@ -1,263 +1,264 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "io"
-  "regexp"
-  "encoding/binary"
-  "encoding/json"
-  "github.com/mediocregopher/radix.v2/redis"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"github.com/mediocregopher/radix.v2/redis"
+	"io"
+	"os"
+	"regexp"
 )
 
 type rutil struct {
-  Host string
-  Port int
-  Auth string
-  cli *redis.Client
+	Host string
+	Port int
+	Auth string
+	cli  *redis.Client
 }
 
 type KeyDump struct {
-  Key    []byte
-  KeyL  uint64
-  Dump   []byte
-  DumpL uint64
-  Pttl   int64
+	Key   []byte
+	KeyL  uint64
+	Dump  []byte
+	DumpL uint64
+	Pttl  int64
 }
 
 type FileHeader struct {
-  Magic [4]byte
-  Version uint8
-  Keys    uint64
+	Magic   [4]byte
+	Version uint8
+	Keys    uint64
 }
 
 func (r *rutil) Client() *redis.Client {
-  if r.cli == nil {
-    cli, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", r.Host, r.Port))
-    checkErr(err)
-    if r.Auth != "" {
-      res := cli.Cmd("AUTH", r.Auth)
-      checkErr(res.Err)
-    }
-    r.cli = cli
-  }
-  return r.cli
+	if r.cli == nil {
+		cli, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", r.Host, r.Port))
+		checkErr(err)
+		if r.Auth != "" {
+			res := cli.Cmd("AUTH", r.Auth)
+			checkErr(res.Err)
+		}
+		r.cli = cli
+	}
+	return r.cli
 }
 
 func (r *rutil) getKeys(wcard string, regex string, invert bool) ([]string, int) {
-  res := r.Client().Cmd("KEYS",wcard)
-  checkErr(res.Err)
-  l, err := res.List()
-  checkErr(err)
-  vsf := make([]string, 0)
-  for _, v := range l {
-    if invertibleMatch(v,regex,invert) {
-      vsf = append(vsf, v)
-    }
-  }
+	res := r.Client().Cmd("KEYS", wcard)
+	checkErr(res.Err)
+	l, err := res.List()
+	checkErr(err)
+	vsf := make([]string, 0)
+	for _, v := range l {
+		if invertibleMatch(v, regex, invert) {
+			vsf = append(vsf, v)
+		}
+	}
 
-  return vsf, len(vsf)
+	return vsf, len(vsf)
 }
 
 func invertibleMatch(s string, r string, v bool) bool {
-  if r == "" { return true }
-  m, _ := regexp.MatchString(r, s);
-  if(v) {
-    return !m
-  } else {
-    return m
-  }
+	if r == "" {
+		return true
+	}
+	m, _ := regexp.MatchString(r, s)
+	if v {
+		return !m
+	} else {
+		return m
+	}
 }
 
 func (r *rutil) dumpKey(k string) KeyDump {
-  d := KeyDump {
-    Key: []byte(k),
-    KeyL: uint64(len(k)),
-  }
-  var err interface{}
-  cli := r.Client()
-  res := cli.Cmd("PTTL",k)
-  checkErr(res.Err)
-  d.Pttl, err = res.Int64()
-  if d.Pttl < 0 {
-    d.Pttl = 0
-  }
-  checkErr(err)
+	d := KeyDump{
+		Key:  []byte(k),
+		KeyL: uint64(len(k)),
+	}
+	var err interface{}
+	cli := r.Client()
+	res := cli.Cmd("PTTL", k)
+	checkErr(res.Err)
+	d.Pttl, err = res.Int64()
+	if d.Pttl < 0 {
+		d.Pttl = 0
+	}
+	checkErr(err)
 
-  res = cli.Cmd("DUMP",k)
-  checkErr(res.Err)
-  d.Dump, err = res.Bytes()
-  checkErr(err)
-  d.DumpL = uint64(len(d.Dump))
+	res = cli.Cmd("DUMP", k)
+	checkErr(res.Err)
+	d.Dump, err = res.Bytes()
+	checkErr(err)
+	d.DumpL = uint64(len(d.Dump))
 
-  return d
+	return d
 }
 
 func (r *rutil) writeHeader(f io.Writer, keys_c int) int {
-  h := FileHeader {
-    Magic:   [4]byte {0x52,0x44,0x4d,0x50},
-    Version: uint8(0x01),
-    Keys: uint64(keys_c),
-  }
+	h := FileHeader{
+		Magic:   [4]byte{0x52, 0x44, 0x4d, 0x50},
+		Version: uint8(0x01),
+		Keys:    uint64(keys_c),
+	}
 
-  checkErr(binary.Write(f, binary.BigEndian, h))
-  return binary.Size(h)
+	checkErr(binary.Write(f, binary.BigEndian, h))
+	return binary.Size(h)
 }
 
 func (r *rutil) readHeader(f io.Reader) FileHeader {
-  var h FileHeader
-  binary.Read(f, binary.BigEndian, &h)
-  return h
+	var h FileHeader
+	binary.Read(f, binary.BigEndian, &h)
+	return h
 }
 
 func (r *rutil) writeDump(f io.Writer, d KeyDump) int {
-  size := binary.Size(d.Pttl)  +
-          binary.Size(d.KeyL)  +
-          binary.Size(d.Key)   +
-          binary.Size(d.DumpL) +
-          binary.Size(d.Dump)
+	size := binary.Size(d.Pttl) +
+		binary.Size(d.KeyL) +
+		binary.Size(d.Key) +
+		binary.Size(d.DumpL) +
+		binary.Size(d.Dump)
 
-  checkErr(binary.Write(f, binary.BigEndian, d.Pttl))
-  checkErr(binary.Write(f, binary.BigEndian, d.KeyL))
-  checkErr(binary.Write(f, binary.BigEndian, d.Key))
-  checkErr(binary.Write(f, binary.BigEndian, d.DumpL))
-  checkErr(binary.Write(f, binary.BigEndian, d.Dump))
-  return size
+	checkErr(binary.Write(f, binary.BigEndian, d.Pttl))
+	checkErr(binary.Write(f, binary.BigEndian, d.KeyL))
+	checkErr(binary.Write(f, binary.BigEndian, d.Key))
+	checkErr(binary.Write(f, binary.BigEndian, d.DumpL))
+	checkErr(binary.Write(f, binary.BigEndian, d.Dump))
+	return size
 }
 
 func (r *rutil) readDump(f io.Reader) KeyDump {
-  var d KeyDump
+	var d KeyDump
 
-  binary.Read(f, binary.BigEndian, &d.Pttl)
-  binary.Read(f, binary.BigEndian, &d.KeyL)
-  d.Key = make([]byte,d.KeyL)
-  binary.Read(f, binary.BigEndian, &d.Key)
-  binary.Read(f, binary.BigEndian, &d.DumpL)
-  d.Dump = make([]byte,d.DumpL)
-  binary.Read(f, binary.BigEndian, &d.Dump)
+	binary.Read(f, binary.BigEndian, &d.Pttl)
+	binary.Read(f, binary.BigEndian, &d.KeyL)
+	d.Key = make([]byte, d.KeyL)
+	binary.Read(f, binary.BigEndian, &d.Key)
+	binary.Read(f, binary.BigEndian, &d.DumpL)
+	d.Dump = make([]byte, d.DumpL)
+	binary.Read(f, binary.BigEndian, &d.Dump)
 
-  return d
+	return d
 }
 
 func (r *rutil) restoreKey(d KeyDump, del bool, ignor bool) int {
-  cli := r.Client()
-  var res *redis.Resp
+	cli := r.Client()
+	var res *redis.Resp
 
-  if del {
-    res = cli.Cmd("DEL",d.Key)
-    checkErr(res.Err)
-  }
+	if del {
+		res = cli.Cmd("DEL", d.Key)
+		checkErr(res.Err)
+	}
 
-  res = cli.Cmd("RESTORE", d.Key, d.Pttl ,d.Dump)
-  if ignor == true {
-    if res.Err != nil {
-      return 0
-    } else {
-      return 1
-    }
-  } else {
-    checkErr(res.Err)
-    return 1
-  }
+	res = cli.Cmd("RESTORE", d.Key, d.Pttl, d.Dump)
+	if ignor == true {
+		if res.Err != nil {
+			return 0
+		} else {
+			return 1
+		}
+	} else {
+		checkErr(res.Err)
+		return 1
+	}
 }
 
 func (r *rutil) printKey(key string, fld []string, json bool) {
-  cli := r.Client()
-  var res *redis.Resp
+	cli := r.Client()
+	var res *redis.Resp
 
-  res = cli.Cmd("TYPE", key)
-  checkErr(res.Err)
-  key_t, err := res.Str()
-  checkErr(err)
+	res = cli.Cmd("TYPE", key)
+	checkErr(res.Err)
+	key_t, err := res.Str()
+	checkErr(err)
 
-  fmt.Printf("KEY: %s\nTYP: %s\n", key, key_t)
-  switch key_t {
-  case "set":
-    res = cli.Cmd("SMEMBERS", key)
-    checkErr(res.Err)
-    set, err := res.List()
-    checkErr(err)
-    fmt.Println("VAL:", set, "\n")
-  case "hash":
-    if len(fld) == 0 {
-      res = cli.Cmd("HGETALL", key)
-      checkErr(res.Err)
-      hash, err := res.Map()
-      checkErr(err)
-      ppHash(hash, json)
-    } else {
-      res = cli.Cmd("HMGET", key, fld)
-      arr, err := res.List()
-      checkErr(err)
-      hash := map[string]string{}
-      for i, k := range fld {
-        hash[k] = arr[i]
-      }
-      ppHash(hash, json)
-    }
-  case "string":
-    res = cli.Cmd("GET",key)
-    checkErr(res.Err)
-    str, err := res.Str()
-    checkErr(err)
-    ppString(str, json)
-  case "zset":
-    res = cli.Cmd("ZRANGE",key, 0, -1)
-    checkErr(res.Err)
-    set, err := res.List()
-    checkErr(err)
-    fmt.Println("VAL:", set, "\n")
-  case "list":
-    res = cli.Cmd("LRANGE",key, 0, -1)
-    checkErr(res.Err)
-    list, err := res.List()
-    checkErr(err)
-    fmt.Println("VAL:", list, "\n")
-  default:
-    checkErr(key_t)
-  }
+	fmt.Printf("KEY: %s\nTYP: %s\n", key, key_t)
+	switch key_t {
+	case "set":
+		res = cli.Cmd("SMEMBERS", key)
+		checkErr(res.Err)
+		set, err := res.List()
+		checkErr(err)
+		fmt.Println("VAL:", set, "\n")
+	case "hash":
+		if len(fld) == 0 {
+			res = cli.Cmd("HGETALL", key)
+			checkErr(res.Err)
+			hash, err := res.Map()
+			checkErr(err)
+			ppHash(hash, json)
+		} else {
+			res = cli.Cmd("HMGET", key, fld)
+			arr, err := res.List()
+			checkErr(err)
+			hash := map[string]string{}
+			for i, k := range fld {
+				hash[k] = arr[i]
+			}
+			ppHash(hash, json)
+		}
+	case "string":
+		res = cli.Cmd("GET", key)
+		checkErr(res.Err)
+		str, err := res.Str()
+		checkErr(err)
+		ppString(str, json)
+	case "zset":
+		res = cli.Cmd("ZRANGE", key, 0, -1)
+		checkErr(res.Err)
+		set, err := res.List()
+		checkErr(err)
+		fmt.Println("VAL:", set, "\n")
+	case "list":
+		res = cli.Cmd("LRANGE", key, 0, -1)
+		checkErr(res.Err)
+		list, err := res.List()
+		checkErr(err)
+		fmt.Println("VAL:", list, "\n")
+	default:
+		checkErr(key_t)
+	}
 }
 
 func ppString(s string, j bool) {
-  if j {
-    var b interface{}
-    err := json.Unmarshal([]byte(s),&b)
-    if err != nil {
-      fmt.Println("VAL:", s, "\n")
-    } else {
-      out, _ := json.MarshalIndent(b,"","\t")
-      fmt.Println(string(out), "\n")
-    }
-  } else {
-    fmt.Println("VAL:", s, "\n")
-  }
+	if j {
+		var b interface{}
+		err := json.Unmarshal([]byte(s), &b)
+		if err != nil {
+			fmt.Println("VAL:", s, "\n")
+		} else {
+			out, _ := json.MarshalIndent(b, "", "\t")
+			fmt.Println(string(out), "\n")
+		}
+	} else {
+		fmt.Println("VAL:", s, "\n")
+	}
 }
 
 func ppHash(h map[string]string, j bool) {
 
-  hh := make(map[string]interface{})
-  for k, v := range h {
-    if j {
-      var b interface{}
-      err := json.Unmarshal([]byte(v), &b)
-      if err != nil {
-        hh[k] = v
-      } else {
-        hh[k] = b
-      }
-    } else {
-      hh[k] = v
-    }
-  }
+	hh := make(map[string]interface{})
+	for k, v := range h {
+		if j {
+			var b interface{}
+			err := json.Unmarshal([]byte(v), &b)
+			if err != nil {
+				hh[k] = v
+			} else {
+				hh[k] = b
+			}
+		} else {
+			hh[k] = v
+		}
+	}
 
-  out, _ := json.MarshalIndent(hh, "", "\t")
-  fmt.Println(string(out),"\n")
+	out, _ := json.MarshalIndent(hh, "", "\t")
+	fmt.Println(string(out), "\n")
 }
 
 func checkErr(err interface{}) {
-  if err != nil {
-    fmt.Println("ERROR:",err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
 }
-
